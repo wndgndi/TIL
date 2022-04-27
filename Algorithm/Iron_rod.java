d1ccceeb62fbbda11f2e89509069cf01b8e70c66
wndgndi <wndgn456@gmail.com>
2022-04-27 19:37:42 +0900

    백준 10799번 쇠막대기 문제풀이

---
+++ Algorithm/Iron_rod.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<String> stack = new Stack<>();
+        String str = br.readLine();    // 문자열 str을 읽는다.
+        char[] arr = str.toCharArray();  // 문자열 str을 char로 하나씩 배열 arr에 넣어준다.
+        int total = 0;
+
+        for (int i = 0; i < arr.length; i++) {  // arr 배열의 길이만큼 반복
+            char s = arr[i];   // arr 배열의 i번째 인덱스에 들어있는 문자 s
+
+            if (s == '(') {     // 배열에 들어가는게 '(' 일 때,
+                stack.push("(");  // 스택을 push한다.
+            } else if (s == ')') {    // 배열에 들어가는게 '('일 경우,
+                stack.pop();   // 스택을 pop한다.
+                if (arr[i - 1] == ')') {   // ')'가 연속으로 나오는 경우,
+                    total++;    // total에 1을 더한다.
+                } else {   // ')'가 연속으로 들어가지 않는 경우,
+                    total += stack.size();   // 스택의 size를 total에 더한다.
+
+                }
+            }
+        }
+        System.out.println(total);
+
+    }
+}
