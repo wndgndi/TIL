099fb135cb7ddcde40abe054c85f6f978d151d22
wndgndi <wndgn456@gmail.com>
2022-04-28 12:24:29 +0900

    백준 10773번 제로 문제풀이

--- Algorithm/Zero.java
+++ Algorithm/Zero.java
@@ -7,21 +7,18 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Stack<Integer> stack = new Stack<>();
 
-        int n = Integer.parseInt(br.readLine());  //  반복 입력받을 횟수 정수로 변환
-        int sum = 0;   // 합을 구하기 위한 변수 sum 
+        int n = Integer.parseInt(br.readLine());  
+        int sum = 0;   
+            int num = Integer.parseInt(br.readLine());   
 
-
-        while(n-- > 0) {   // n이 0보다 클 동안
-            int num = Integer.parseInt(br.readLine());   // 정수를 반복 입력받는다.
-
-            if(!stack.isEmpty() && num == 0) {   // 스택이 비어있지 않고 0을 입력받으면
-               stack.pop();   // 스택을 pop 한다
+            if(!stack.isEmpty() && num == 0) {  
+               stack.pop();  
             }
-            else {stack.push(num);}    // 0이 아닌 정수를 입력받으면 스택에 푸시한다
+            else {stack.push(num);}    
 
         }
-        for(int total : stack) {  // 스택의 정수들을 total에 하나씩 넣어준다
-            sum += total;   // 하나씩 들어가는 total을 sum으로 합을 구한다.
+        for(int total : stack) { 
+            sum += total;  
         }
 
         bw.write(sum+"\n");
@@ -30,3 +27,11 @@ public class Main {
 
     }
 }
+
+
+
+﻿
+숫자를 입력받아 0이 아니면 스택에 하나씩 push 해주고, 0을 입력받으면 스택을 pop 해준다.
+그리고 마지막에 스택의 값들을 모두 더해주면 되는 간단한 문제이다.
+
+﻿
