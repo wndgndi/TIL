3b14b323d981ae1ab819e8ddae09d9078a23bebe
wndgndi <wndgn456@gmail.com>
2022-05-14 03:57:29 +0900

    docs: 백준 11656 접미사 배열 문제풀이

--- Algorithm/Suffix.java
+++ Algorithm/Suffix.java
@@ -1,3 +1,11 @@
+11656번 문제풀이
+
+접미사 배열은 문자열 S의 모든 접미사를 사전순으로 정렬해 놓은 배열이다.
+baekjoon의 접미사는 baekjoon, aekjoon, ekjoon, kjoon, joon, oon, on, n 으로 총 8가지가 있고, 이를 사전순으로 정렬하면, aekjoon, baekjoon, ekjoon, joon, kjoon, n, on, oon이 된다.
+문자열 S가 주어졌을 때, 모든 접미사를 사전순으로 정렬한 다음 출력하는 프로그램을 작성하시오.​
+
+
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.LinkedList;
