26c792a79fc3d079dab141255f0d72d60a3be3d3
wndgndi <wndgn456@gmail.com>
2022-06-15 17:27:24 +0900

    docs: 백준 10815 숫자 카드 문제풀이

--- Algorithm/CardNum.java
+++ Algorithm/CardNum.java
@@ -1,3 +1,8 @@
+10815번 문제
+
+숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 가지고 있는지 아닌지를 구하는 프로그램을 작성하시오.
+
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
