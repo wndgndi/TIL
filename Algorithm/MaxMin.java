407becaa329376c681a536af0fa1b0e08380b1ee
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-13 16:34:43 +0900

    백준 20053 최소,최대2 문제풀이

---
+++ Algorithm/MaxMin.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());  // 테스트 케이스의 개수 입력
+
+        for(int i=0; i<n; i++) {   // n번만큼 반복
+            int a = Integer.parseInt(br.readLine());   // 정수의 개수 a 입력
+            StringTokenizer st = new StringTokenizer(br.readLine());  // 문자열 형태의 정수를 공백으로 구분해서 입력
+            int[] arr = new int[a];    // a 크기의 정수 배열 생성
+            for(int j=0; j<a; j++) {    // a번만큼 반복
+                arr[j] = Integer.parseInt(st.nextToken());  // st의 토큰이 없을때까지 정수로 변환하여 arr에 넣어줌
+            }
+            Arrays.sort(arr);   // 배열 arr 정렬
+            System.out.println(arr[0] + " " + arr[a-1]);  
+        }
+    }
+}
