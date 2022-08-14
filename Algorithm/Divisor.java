aecb3654d7895438ed9a5a6815aea2bad8216704
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-08-14 20:04:25 +0900

    백준 2501 약수 구하기 문제풀이

--- Algorithm/Divisor.java
+++ Algorithm/Divisor.java
@@ -1,3 +1,19 @@
+2501번 문제
+
+어떤 자연수 p와 q가 있을 때, 만일 p를 q로 나누었을 때 나머지가 0이면 q는 p의 약수이다. 
+6을 예로 들면
+
+●   6 ÷ 1 = 6 … 0
+●   6 ÷ 2 = 3 … 0
+●   6 ÷ 3 = 2 … 0
+●   6 ÷ 4 = 1 … 2
+●   6 ÷ 5 = 1 … 1
+●   6 ÷ 6 = 1 … 0
+
+그래서 6의 약수는 1, 2, 3, 6, 총 네 개이다.
+두 개의 자연수 N과 K가 주어졌을 때, N의 약수들 중 K번째로 작은 수를 출력하는 프로그램을 작성하시오.
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
