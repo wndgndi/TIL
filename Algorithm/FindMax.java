a625c860506691c1d75f9e317fe3a755ec648e7b
wndgndi <wndgn456@gmail.com>
2023-01-08 12:24:27 +0900

    Lv0. 가장 큰 수 찾기 문제풀이

--- Algorithm/FindMax.java
+++ Algorithm/FindMax.java
@@ -3,7 +3,7 @@ Lv0. 가장 큰 수 찾기 문제
 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 
 
-< 코드 >
+< 코드 Java >
 
 import java.util.*;
 import java.util.stream.Stream;
@@ -19,3 +19,16 @@ class Solution {
         return answer;
     }
 }
+
+
+< 코드 Python >
+
+def solution(array):
+    answer = [0]*2
+    
+    for i in range(len(array)):
+        if array[i] > answer[0]:
+            answer[0] = array[i]
+            answer[1] = i       
+    
+    return answer
