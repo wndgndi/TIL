c928714b6c824494667889b30586e633b08742bd
wndgndi <wndgn456@gmail.com>
2023-01-15 01:53:28 +0900

    Lv0. 배열의 평균값 문제풀이

--- Algorithm/ArrayAverage.java
+++ Algorithm/ArrayAverage.java
@@ -3,7 +3,7 @@ Lv0. 배열의 평균값
 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
 
 
-< 코드 >
+< 코드1 Java >
 
 import java.util.*;
 
@@ -14,5 +14,28 @@ class Solution {
         return answer;
     }
 }
-    
+
+< 코드2 Java >
+
+import java.util.*;
+
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = Arrays.stream(numbers).average().orElse(0);
+        
+        return answer;
+    }
 }
+
+< 코드1 Python >
+
+def solution(numbers):
+    answer = sum(numbers)/len(numbers)
+    return answer
+
+
+< 코드2 Python >
+
+def solution(numbers):
+    answer = np.mean(numbers)
+    return answer
