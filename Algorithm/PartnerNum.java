13c94899a568648e5307c19050d40a55b5b60ebb
wndgndi <wndgn456@gmail.com>
2023-02-23 19:26:59 +0900

    Lv1. 숫자 짝궁 문제풀이

--- Algorithm/PartnerNum.java
+++ Algorithm/PartnerNum.java
@@ -18,37 +18,37 @@ import java.util.stream.Collectors;
 class Solution {
     public String solution(String X, String Y) {
         String answer = "";
-        String[] strx = X.split("");
-        String[] stry = Y.split("");
+        String[] strx = X.split("");   // 문자열 X 나누어서 배열에 담아줌
+        String[] stry = Y.split("");   // 문자열 Y 나누어서 배열에 담아줌
         List<String> list = new ArrayList<>();
-        List<String> listx = Arrays.stream(strx).distinct().collect(Collectors.toList());
-        List<String> listy = Arrays.stream(stry).distinct().collect(Collectors.toList());
+        List<String> listx = Arrays.stream(strx).distinct().collect(Collectors.toList());  // 중복 걸러낸 리스트
+        List<String> listy = Arrays.stream(stry).distinct().collect(Collectors.toList());  // 중복 걸러낸 리스트
         
-        for(int i=0; i<listx.size(); i++) {
-            if(listy.contains(listx.get(i))) {
-                int a = Collections.frequency(Arrays.asList(strx), listx.get(i));
-                int b = Collections.frequency(Arrays.asList(stry), listx.get(i));
-                for(int j=0; j<Math.min(a,b); j++) {
-                    list.add(listx.get(i));
+        for(int i=0; i<listx.size(); i++) {    // listx의 크기만큼 반복
+            if(listy.contains(listx.get(i))) {   // listy에 listx의 짝궁이 있다면,
+                int a = Collections.frequency(Arrays.asList(strx), listx.get(i));   // strx에 포함된 해당 정수 개수를 구함
+                int b = Collections.frequency(Arrays.asList(stry), listx.get(i));   // stry에 포함된 해당 정수 개수를 구함
+                for(int j=0; j<Math.min(a,b); j++) {   // a와 b 중 작은 값이 공통된 개수이므로 그만큼 반복
+                    list.add(listx.get(i));  // list에 숫자 짝궁을 넣어줌
                 }
             }
         }
         
-        Collections.sort(list, Collections.reverseOrder());
+        Collections.sort(list, Collections.reverseOrder());   // 가장 큰 정수를 만들어야하므로 내림차순 정렬
         
-        if(list.size() == 0) {
-            answer = "-1";
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i<list.size(); i++) {
-                sb.append(list.get(i));
+        if(list.size() == 0) {   // list의 크기가 0이면,
+            answer = "-1";  // answer은 "-1"이 됨
+        } else {   // list의 크기가 0이 아니면,
+            StringBuilder sb = new StringBuilder();   
+            for(int i=0; i<list.size(); i++) {   // list의 크기만큼 반복
+                sb.append(list.get(i));    // sb에 list의 i번 인덱스를 넣어줌
             }
-            answer = sb.toString();
+            answer = sb.toString();   // sb를 문자열로 answer에 넣어줌
         }
         
-        if(answer.charAt(0) == '0' && answer.charAt(1) == '0') {
-            answer = "0";
+        if(answer.charAt(0) == '0' && answer.charAt(1) == '0') {  // answer의 첫번째, 두번째 숫자 모두 '0'일 경우,
+            answer = "0";   // answer은 "0"이 됨
         }
-        return answer;
+        return answer;   // answer를 반환
     }
 }
