764e5b7ca7a1c53bea6c8e70094b6eaf789c5a83
wndgndi <wndgn456@gmail.com>
2023-03-04 23:58:13 +0900

    백준 108111 바구니 뒤집기 문제풀이

---
+++ Algorithm/SwitchBasket.java
@@ -0,0 +1,62 @@
+백준 10811 바구니 뒤집기 문제 
+
+도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 순서대로 적혀져 있다. 바구니는 일렬로 놓여져 있고, 가장 왼쪽 바구니를 1번째 바구니, 그 다음 바구니를 2번째 바구니, ..., 가장 오른쪽 바구니를 N번째 바구니라고 부른다. 
+
+도현이는 앞으로 M번 바구니의 순서를 역순으로 만들려고 한다. 도현이는 한 번 순서를 역순으로 바꿀 때, 순서를 역순으로 만들 범위를 정하고, 그 범위에 들어있는 바구니의 순서를 역순으로 만든다.
+
+바구니의 순서를 어떻게 바꿀지 주어졌을 때, M번 바구니의 순서를 역순으로 만든 다음, 바구니에 적혀있는 번호를 가장 왼쪽 바구니부터 출력하는 프로그램을 작성하시오.
+
+
+< 코드 Java >
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int tmp = 1;
+
+        for(int i=0; i<n; i++) {
+            arr[i] = tmp;
+            tmp++;
+        }
+        
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int[] arr2 = Arrays.copyOfRange(arr, a-1, b);
+            List<Integer> list = Arrays.stream(arr2).boxed().collect(Collectors.toList());
+            Collections.reverse(list);
+            tmp = 0;
+
+            for(int j=a-1; j<b; j++) {
+                arr[j] = list.get(tmp);
+                tmp++;
+            }
+        }
+
+        for(int i :arr) {
+            bw.write(i + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
