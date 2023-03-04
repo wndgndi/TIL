ff8333477deec1d949bd4437a0db78ee80835795
wndgndi <wndgn456@gmail.com>
2023-03-05 01:00:21 +0900

    백준 10811 바구니 뒤집기 문제풀이

--- Algorithm/SwitchBasket.java
+++ Algorithm/SwitchBasket.java
@@ -1,13 +1,68 @@
 백준 10811 바구니 뒤집기 문제 
 
-도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 순서대로 적혀져 있다. 바구니는 일렬로 놓여져 있고, 가장 왼쪽 바구니를 1번째 바구니, 그 다음 바구니를 2번째 바구니, ..., 가장 오른쪽 바구니를 N번째 바구니라고 부른다. 
+도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 순서대로 적혀져 있다. 
+바구니는 일렬로 놓여져 있고, 가장 왼쪽 바구니를 1번째 바구니, 그 다음 바구니를 2번째 바구니, ..., 가장 오른쪽 바구니를 N번째 바구니라고 부른다. 
 
 도현이는 앞으로 M번 바구니의 순서를 역순으로 만들려고 한다. 도현이는 한 번 순서를 역순으로 바꿀 때, 순서를 역순으로 만들 범위를 정하고, 그 범위에 들어있는 바구니의 순서를 역순으로 만든다.
 
 바구니의 순서를 어떻게 바꿀지 주어졌을 때, M번 바구니의 순서를 역순으로 만든 다음, 바구니에 적혀있는 번호를 가장 왼쪽 바구니부터 출력하는 프로그램을 작성하시오.
 
 
-< 코드 Java >
+< 코드1 Java >
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());  
+        int[] arr = new int[n];   
+        int tmp = 1; 
+
+        for(int i=0; i<n; i++) {
+            arr[i] = tmp;
+            tmp++;
+        }
+        
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int[] arr2 = Arrays.copyOfRange(arr, a-1, b);   // arr의 a-1부터 b-1번 인덱스 복사하여 배열 만듬
+            List<Integer> list = Arrays.stream(arr2).boxed().collect(Collectors.toList());  // arr2 를 리스트로 변환
+            Collections.reverse(list);   // list 뒤집어줌
+            tmp = 0;
+
+            for(int j=a-1; j<b; j++) {
+                arr[j] = list.get(tmp);   // list의 값들을 꺼내서 arr의 j번 인덱스에 넣어줌
+                tmp++;  // tmp가 1 증가
+            }
+        }
+
+        for(int i :arr) {
+            bw.write(i + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
+
+< 코드2 Java >
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -28,9 +83,9 @@ public class Main {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        int tmp = 1;
+        int m = Integer.parseInt(st.nextToken());  
+        int[] arr = new int[n];   
+        int tmp = 1; 
 
         for(int i=0; i<n; i++) {
             arr[i] = tmp;
@@ -42,14 +97,14 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            int[] arr2 = Arrays.copyOfRange(arr, a-1, b);
-            List<Integer> list = Arrays.stream(arr2).boxed().collect(Collectors.toList());
-            Collections.reverse(list);
+            int[] arr2 = Arrays.copyOfRange(arr, a-1, b);   // arr의 a-1부터 b-1번 인덱스 복사하여 배열 만듬
+            List<Integer> list = Arrays.stream(arr2).boxed().collect(Collectors.toList());  // arr2 를 리스트로 변환
+            Collections.reverse(list);   // list 뒤집어줌
             tmp = 0;
 
             for(int j=a-1; j<b; j++) {
-                arr[j] = list.get(tmp);
-                tmp++;
+                arr[j] = list.get(tmp);   // list의 값들을 꺼내서 arr의 j번 인덱스에 넣어줌
+                tmp++;  // tmp가 1 증가
             }
         }
 
