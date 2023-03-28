0c781db31681971333001aff410528dafcea5236
wndgndi <wndgn456@gmail.com>
2023-03-28 12:38:32 +0900

    백준 1436 영화감독 숌 문제풀이

---
+++ Algorithm/Director.java
@@ -0,0 +1,49 @@
+1436번 문제 
+
+666은 종말을 나타내는 수라고 한다. 따라서, 많은 블록버스터 영화에서는 666이 들어간 제목을 많이 사용한다. 영화감독 숌은 세상의 종말 이라는 시리즈 영화의 감독이다. 
+조지 루카스는 스타워즈를 만들 때, 스타워즈 1, 스타워즈 2, 스타워즈 3, 스타워즈 4, 스타워즈 5, 스타워즈 6과 같이 이름을 지었고, 피터 잭슨은 반지의 제왕을 만들 때, 반지의 제왕 1, 반지의 제왕 2, 반지의 제왕 3과 같이 영화 제목을 지었다. 
+하지만 숌은 자신이 조지 루카스와 피터 잭슨을 뛰어넘는다는 것을 보여주기 위해서 영화 제목을 좀 다르게 만들기로 했다.
+
+종말의 수란 어떤 수에 6이 적어도 3개 이상 연속으로 들어가는 수를 말한다. 제일 작은 종말의 수는 666이고, 그 다음으로 큰 수는 1666, 2666, 3666, .... 이다. 
+따라서, 숌은 첫 번째 영화의 제목은 "세상의 종말 666", 두 번째 영화의 제목은 "세상의 종말 1666"와 같이 이름을 지을 것이다. 일반화해서 생각하면, N번째 영화의 제목은 세상의 종말 (N번째로 작은 종말의 수) 와 같다.
+
+숌이 만든 N번째 영화의 제목에 들어간 수를 출력하는 프로그램을 작성하시오. 숌은 이 시리즈를 항상 차례대로 만들고, 다른 영화는 만들지 않는다.
+
+
+< 코드 Java >
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int movie = 666;   // 숌의 영화 시리즈
+        int cnt = 1;   // n번째 영화를 구하기 위한 변수
+        int value = 666;   // 값을 증가시키며 종말의 수를 찾을 변수
+
+        while(true) {   // n번째 영화를 찾을 때까지 반복
+            if(cnt == n) {   // cnt가 n과 같으면,
+                bw.write(movie + "\n");   // movie를 출력
+                break;   // 반복문 탈출
+            }
+            ++value;   // 값을 증가시킴
+            String check = String.valueOf(value);   // 정수를 문자열로 변환
+            if(check.contains("666")) {   // 문자열에 "666"이 포함되어 있으면,
+                movie = value;   // movie에 value 값을 넣어줌
+                cnt++;   // cnt 값을 증가시켜줌
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
