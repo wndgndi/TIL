94d1338bc3dbee038a11071193fd59796f6f7a3b
wndgndi <wndgn456@gmail.com>
2023-04-03 16:41:59 +0900

    백준 2745 진법 변환 문제풀이

---
+++ Algorithm/BaseConversion.java
@@ -0,0 +1,33 @@
+백준 2745번 문제
+
+B진법 수 N이 주어진다. 이 수를 10진법으로 바꿔 출력하는 프로그램을 작성하시오.
+
+10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+
+A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
+
+
+< 코드 Java >
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String str = st.nextToken();
+
+        bw.write(Integer.parseInt(str, Integer.parseInt(st.nextToken())) + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+}
