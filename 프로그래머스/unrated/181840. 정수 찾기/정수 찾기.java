8a5ea2e305daa6b409e0b3aa130b94fd9b3ed4b9
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-05-10 18:38:08 +0900

    [unrated] Title: 정수 찾기, Time: 1.23 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -2,16 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
-
-        int a = Arrays.stream(num_list).filter(i -> n == i).findFirst().orElse(-1);
-              
-        if(a != -1) {
-            answer = 1;
-        } else {
-            answer = 0;
-        }
         
-        return answer;
+        return Arrays.stream(num_list).anyMatch(i -> i == n)? 1:0;
     }
 }
