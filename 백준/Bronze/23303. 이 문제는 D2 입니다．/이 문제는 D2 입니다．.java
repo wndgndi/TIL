573c9be345f02e82fef34d51d832e008972a633f
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-07-10 19:02:04 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 132 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String name = br.readLine();
+
+        if(name.contains("d2") || name.contains("D2")) {
+            bw.write("D2");
+        } else {
+            bw.write("unrated");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
