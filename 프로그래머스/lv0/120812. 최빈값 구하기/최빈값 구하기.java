d0be6645a9d66491c8b39d1676932b2005009bfc
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-07-30 13:51:49 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.11 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -4,35 +4,18 @@ import java.util.stream.*;
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        Arrays.sort(array);
-        map.put(array[0], 1);
-        
-        int cnt = 1;
         int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i=1; i<array.length; i++) {
-            if(array[i] != array[i-1]) {
-                cnt = 0;
+        for(int num:array) {
+            int cnt = map.getOrDefault(num,0) + 1;
+            if(cnt > max) {
+                max = cnt;
+                answer = num;
+            } else if(cnt == max) {
+                answer = -1;
             }
-            cnt++;
-            map.put(array[i], cnt);
-        }
-
-        cnt = 1;
-                
-        for(Map.Entry<Integer, Integer> entry:map.entrySet()) {
-            if(entry.getValue() > max) {
-                max = entry.getValue();
-                answer = entry.getKey();
-                cnt = 1;
-            } else if(entry.getValue() == max) {
-                cnt++;
-            }
-        }
-        
-        if(cnt != 1) {
-            answer = -1;
+            map.put(num, cnt);
         }
         
         return answer;
