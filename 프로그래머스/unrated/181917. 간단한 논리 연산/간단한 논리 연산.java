62328d620247083616c49f41b8d1a79bc36bab88
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-07-31 17:57:12 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,25 +1,5 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = true;
-        
-        if(x1 || x2) {
-            x1 = true;
-        } else {
-            x1 = false;
-        }
-        
-        if(x3 || x4) {
-            x2 = true;
-        } else {
-            x2 = false;
-        }
-        
-        if(x1 && x2) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-        
-        return answer;
+        return (x1||x2) && (x3||x4);
     }
 }
