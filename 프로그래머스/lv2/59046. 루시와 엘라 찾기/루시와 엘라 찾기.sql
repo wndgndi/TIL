fe8517ffcad80e0b8a8f904648b48d78d792675a
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-08-11 12:03:55 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,3 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE 
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME IN('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
-ORDER BY ANIMAL_ID
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle','Rogan', 'Sabrina', 'Mitty')
