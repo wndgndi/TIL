b99caa377c32cb95793b8d8c40333851bdd0c31d
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-08-21 12:28:34 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,11 +1,24 @@
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        if(str1.contains(str2)) {
-            answer = 1;
-        } else {
+        int idx = 0;
+        
+        for(int i=0; i<str1.length(); i++) {
+            if(str1.charAt(i) == str2.charAt(idx)) {
+                if(idx == str2.length()-1) {
+                    answer = 1;
+                    break;
+                }
+                idx++;
+            } else {
+                idx = 0;
+            }
+        }
+        
+        if(answer == 0) {
             answer = 2;
         }
+        
         return answer;
     }
 }
