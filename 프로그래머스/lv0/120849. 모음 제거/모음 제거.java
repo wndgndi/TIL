e748751a89f542c37a900245b50adbb365752338
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-08-23 10:53:27 +0900

    [level 0] Title: 모음 제거, Time: 0.18 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,18 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            
-            if('a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c) {
-                continue;
-            } else {
-                sb.append(c);
-            }
-        }
-        answer = sb.toString();
+        String answer = my_string.replaceAll("a|e|i|o|u", "");
         return answer;
     }
 }
