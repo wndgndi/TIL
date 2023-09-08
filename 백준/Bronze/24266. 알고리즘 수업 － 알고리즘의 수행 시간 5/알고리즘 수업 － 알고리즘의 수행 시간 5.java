89d39f885fdbcb186d2e92e22f6d0ec9a014a9d1
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-09-08 16:50:13 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 140 ms, Memory: 15788 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Integer.parseInt(br.readLine());
+
+        bw.write(n*n*n + "\n" + 3);
+        bw.flush();
+        bw.close();
+    }
+
+}
