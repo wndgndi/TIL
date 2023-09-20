537599061eb1b3b982a051100e8434c8b167256a
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-09-20 21:09:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 53.76 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = -1;
         Stack<Character> stack = new Stack<>();
         
         for(int i=0; i<s.length(); i++) {
@@ -13,12 +12,6 @@ class Solution {
             }  
         }
 
-        if(stack.isEmpty()) {
-            answer = 1;    
-        } else {
-            answer = 0;
-        }
-        
-        return answer;
+        return stack.isEmpty() ? 1 : 0;
     }
 }
