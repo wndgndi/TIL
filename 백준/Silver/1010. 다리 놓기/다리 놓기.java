779b691949639dab0a020b010252e965ca8c7440
wndgndi <102509248+wndgndi@users.noreply.github.com>
2023-12-06 23:55:28 +0900

    [Silver V] Title: 다리 놓기, Time: 156 ms, Memory: 14796 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int dp[][] = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            bw.write(combi(m, n) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int combi(int n, int r) {
+        if(dp[n][r] > 0) {
+            return dp[n][r];
+        }
+
+        if(n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+
+        return dp[n][r] = combi(n-1, r-1) + combi(n-1, r);
+    }
+
+}
