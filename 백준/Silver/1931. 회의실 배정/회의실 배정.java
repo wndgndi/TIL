8a383ff0fd128f156bbedba89a1873962e5b6347
wndgndi <102509248+wndgndi@users.noreply.github.com>
2024-03-06 18:20:34 +0900

    [Silver I] Title: 회의실 배정, Time: 544 ms, Memory: 44308 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+    int[][] arr = new int[n][2];
+
+    for(int i=0; i<n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+
+      arr[i][0] = Integer.parseInt(st.nextToken());
+      arr[i][1] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(arr, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] o1, int[] o2) {
+        if(o1[1] - o2[1] == 0) {
+          return o1[0] - o2[0];
+        }
+
+        return o1[1] - o2[1];
+      }
+    });
+
+    int start = arr[0][0];
+    int end = arr[0][1];
+    int cnt = 1;
+
+    for(int i=1; i<arr.length; i++) {
+      start = arr[i][0];
+
+      if(end <= start) {
+        cnt++;
+        end = arr[i][1];
+      }
+    }
+
+    bw.write(cnt + "\n");
+    bw.flush();
+    bw.close();
+  }
+}
