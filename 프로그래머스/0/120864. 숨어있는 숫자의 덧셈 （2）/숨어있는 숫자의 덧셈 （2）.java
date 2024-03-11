48b9f05b79334102f2b58d12199152df46e6275c
wndgndi <102509248+wndgndi@users.noreply.github.com>
2024-03-11 15:56:39 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.42 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        String[] numbers = my_string.split("\\D+");
+        System.out.println(Arrays.toString(numbers));
+        
+        for(int i=0; i<numbers.length; i++) {
+            if(!numbers[i].equals("")) {
+                answer += Integer.parseInt(numbers[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
