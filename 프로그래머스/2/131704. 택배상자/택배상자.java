8f3492b168b8852b408dfccf9ec9d5dd75324fe5
wndgndi <102509248+wndgndi@users.noreply.github.com>
2024-03-14 01:00:12 +0900

    [level 2] Title: 택배상자, Time: 212.98 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,44 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
+        // `sub` 스택은 보조로 사용됩니다.
         Stack<Integer> sub = new Stack<>();
-        Queue<Integer> yj = new LinkedList<>();
-        List<Integer> dlvy = new LinkedList<>();
-        
-        for(int i=1; i<=order.length; i++) {
-            yj.add(i);
-            dlvy.add(order[i-1]);
-        }
-                    
-        loop:
-        for(int i=0; i<order.length; i++) {
-            while(true) {
-                if(!yj.isEmpty()) {
-                    if(yj.peek().intValue() == dlvy.get(0)) {
-                        yj.poll();
-                        dlvy.remove(0);
-                        answer++;
-                    } else {
-                        sub.push(yj.poll());
-                    }
-                }
-                
-                if(answer == order.length) {
-                    break loop;
-                }
-                           
-                if(!sub.isEmpty()) {
-                    if(sub.peek().intValue() != dlvy.get(0)) {
-                        break;
-                    } else {
-                        sub.pop();
-                        dlvy.remove(0);
-                        answer++;
-                    }
-                } 
+        // `yjIndex`는 현재 처리해야 할 상자의 순서를 가리킵니다.
+        int yjIndex = 1;
+
+        for (int i = 0; i < order.length; i++) {
+            // 현재 주문이 창고의 순서와 일치할 때까지 상자를 `sub`로 보냅니다.
+            while (yjIndex <= order.length && (sub.isEmpty() || sub.peek() != order[i])) {
+                sub.push(yjIndex);
+                yjIndex++;
             }
-        }                        
-            
+
+            // `sub` 스택의 상자가 택배 기사가 필요한 상자와 일치하는 경우,
+            // 해당 상자를 처리하고 `answer`를 증가시킵니다.
+            if (!sub.isEmpty() && sub.peek() == order[i]) {
+                sub.pop();
+                answer++;
+            } else {
+                // 현재 상자를 처리할 수 없는 경우, 더 이상의 처리가 불가능하므로 반복을 종료합니다.
+                break;
+            }
+        }
+
         return answer;
     }
 }
