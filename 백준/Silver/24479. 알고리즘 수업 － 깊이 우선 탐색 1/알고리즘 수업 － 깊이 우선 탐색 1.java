b43c39f6b7a6bd8d35999eb3cfaf749826b97643
wndgndi <102509248+wndgndi@users.noreply.github.com>
2024-03-18 20:56:19 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1132 ms, Memory: 94300 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -9,54 +9,57 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int cnt = 1;
-    static List<List<Integer>> list = new ArrayList<>();
-    static int[] arr;
+  static int[] check;
+  static int order;
+  static List<List<Integer>> list = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-        arr = new int[n+1];
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int r = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<=n; i++) {
-            list.add(new ArrayList<Integer>());
-        }
+    check = new int[n+1];
 
-        for(int i=1; i<=m; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            list.get(a).add(b);
-            list.get(b).add(a);
-        }
-
-        for(int i=0; i<list.size(); i++) {
-            Collections.sort(list.get(i));
-        }
-        arr[v] = cnt;
-        DFS(v);
-
-        for(int i=1; i<arr.length; i++) {
-            bw.write(arr[i] + "\n");
-        }
-        bw.flush();
-        bw.close();
+    for(int i=0; i<=n; i++) {
+      list.add(new ArrayList<Integer>());
     }
-    
-    public static void DFS(int start) {
-        for(int i=0; i<list.get(start).size(); i++) {
-            if(list.get(start).get(i) != 0 && arr[list.get(start).get(i)] == 0) {
-                arr[list.get(start).get(i)] = ++cnt;
-                DFS(list.get(start).get(i));
-            }
-        }
+
+    for(int i=0; i<m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+
+      list.get(u).add(v);
+      list.get(v).add(u);
     }
+
+    for(int i=1; i<list.size(); i++) {
+      Collections.sort(list.get(i));
+    }
+
+    order = 1;
+
+    dfs(r);
+
+    for(int i=1; i<check.length; i++) {
+      bw.write(check[i] + "\n");
+    }
+    bw.flush();
+    bw.close();
+  }
+
+  public static void dfs(int v) {
+      check[v] = order;
+
+      for(int i=0; i<list.get(v).size(); i++) {
+        if(list.get(v).get(i) != 0 && check[list.get(v).get(i)] == 0) {
+          order++;
+          dfs(list.get(v).get(i));
+        }
+      }
+  }
 }
