5cc91376ce8bd913b592494d7ff4255a5a771032
wndgndi <102509248+wndgndi@users.noreply.github.com>
2024-04-10 21:27:07 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int width = (brown - 2) / 2;
+        int length = 1;
+        
+        while(true) {
+            if(((width - 2) % yellow == 0 || yellow % (width - 2) == 0) 
+               && (width - 2) * length == yellow) {
+                answer[0] = width;
+                answer[1] = length + 2;
+                break;
+            } else {
+                width--;
+                length++;
+            }
+        }
+
+        return answer;
+    }
+}
