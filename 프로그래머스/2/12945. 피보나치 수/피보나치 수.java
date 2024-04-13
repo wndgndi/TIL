da5455f77f5e51542a36c17fde6a6f6778a95130
wndgndi <102509248+wndgndi@users.noreply.github.com>
2024-04-13 14:23:55 +0900

    [level 2] Title: 피보나치 수, Time: 2.29 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        
+        for(int i=2; i<=n; i++) {
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+        }
+        
+        return arr[n];
+    }
+}
