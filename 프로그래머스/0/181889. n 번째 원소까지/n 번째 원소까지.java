8da088bbbdd4c00e3acdb83946f159e44ae6829f
wndgndi <102509248+wndgndi@users.noreply.github.com>
2024-07-10 22:34:38 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,11 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[n];
-        
-        for(int i=0; i<n; i++) {
-            answer[i] = num_list[i];
-        }
-        
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, n);
     }
 }
